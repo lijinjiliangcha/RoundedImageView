0ad421f20f266897b497433d96a0d000aff50a59
sheng <lijinjiliangcha@gmail.com>
2020-06-02 17:40:42 +0800

    修正一个导致崩溃的bug

--- roundedimageview/src/main/java/com/lijinjiliangcha/roundedimageview/RoundedImageView.kt
+++ roundedimageview/src/main/java/com/lijinjiliangcha/roundedimageview/RoundedImageView.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.widget.AppCompatImageView
 //圆弧ImageView
 class RoundedImageView : AppCompatImageView {
 
-    private var path: Path? = null
+    //    private var path: Path? = null
     private var radius: Float = 0f
 
     private var roundedStyle: Int = 0
@@ -81,47 +81,45 @@ class RoundedImageView : AppCompatImageView {
             return
         }
         val path = Path()
-        path.let {
-            val r = 2 * radius
-            it.moveTo(0f, radius)
-            //左上角
-            val ltF = RectF(0f, 0f, r, r)
-            it.arcTo(ltF, 180f, 90f, false)
-            //右上角
-            val rtF = RectF(w.toFloat() - r, 0f, w.toFloat(), r)
-            it.arcTo(rtF, 270f, 90f, false)
-            //右下角
-            val rbF = RectF(w.toFloat() - r, h.toFloat() - r, w.toFloat(), h.toFloat())
-            it.arcTo(rbF, 0f, 90f, false)
-            //左下角
-            val lbF = RectF(0f, h.toFloat() - r, r, h.toFloat())
-            it.arcTo(lbF, 90f, 90f, false)
-            createClipBitmap(w, h)
-        }
+        val r = 2 * radius
+        path.moveTo(0f, radius)
+        //左上角
+        val ltF = RectF(0f, 0f, r, r)
+        path.arcTo(ltF, 180f, 90f, false)
+        //右上角
+        val rtF = RectF(w.toFloat() - r, 0f, w.toFloat(), r)
+        path.arcTo(rtF, 270f, 90f, false)
+        //右下角
+        val rbF = RectF(w.toFloat() - r, h.toFloat() - r, w.toFloat(), h.toFloat())
+        path.arcTo(rbF, 0f, 90f, false)
+        //左下角
+        val lbF = RectF(0f, h.toFloat() - r, r, h.toFloat())
+        path.arcTo(lbF, 90f, 90f, false)
+        createClipBitmap(w, h, path)
     }
 
     private fun onCircular(w: Int, h: Int) {
-        path = Path()
+        val path = Path()
         val cx = w.toFloat() / 2
         val cy = h.toFloat() / 2
-        path?.addCircle(cx, cy, if (cx > cy) cy else cx, Path.Direction.CW)
-        createClipBitmap(w, h)
+        path.addCircle(cx, cy, if (cx > cy) cy else cx, Path.Direction.CW)
+        createClipBitmap(w, h, path)
     }
 
     private fun onOval(w: Int, h: Int) {
-        path = Path()
-        path?.addOval(0f, 0f, w.toFloat(), h.toFloat(), Path.Direction.CW)
-        createClipBitmap(w, h)
+        val path = Path()
+        path.addOval(0f, 0f, w.toFloat(), h.toFloat(), Path.Direction.CW)
+        createClipBitmap(w, h, path)
     }
 
-    private fun createClipBitmap(w: Int, h: Int) {
+    private fun createClipBitmap(w: Int, h: Int, path: Path) {
         rect = Rect(0, 0, w, h)
         rectF = RectF(0f, 0f, w.toFloat(), h.toFloat())
         clipBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(clipBitmap!!)
         // 画布绘制Bitmap时搞锯齿
         canvas.setDrawFilter(PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG))
-        canvas.drawPath(path!!, clipPaint)
+        canvas.drawPath(path, clipPaint)
     }
 
     override fun onDraw(canvas: Canvas?) {
